b0494aaf1c870e8dfdffa5e7545d87c8e1af7ea9
George Kelley <geokelley@gmail.com>
2019-09-20 14:12:11 -0500

    added ERD pdf to the package, which I forgot to include in the first commit

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,6 +1,6 @@
 const API = {
     getData() {
-        return fetch("http://localhost:8088/employees?_expand=computer&_expand=department")
+        return fetch("http://localhost:8088/departments?_embed=computers&_embed=employees")
             .then(response => response.json())
     }
 }
